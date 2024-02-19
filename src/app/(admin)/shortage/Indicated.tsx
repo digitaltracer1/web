04b4722f317a5a36e79c133dfd53b8a372447ee6
tiagoribeironfs1 <tiagoribeironfs1@gmail.com>
2024-02-19 12:18:33 -0300

    feat: create page indicated

--- src/app/(admin)/shortage/Indicated.tsx
+++ src/app/(admin)/shortage/Indicated.tsx
@@ -1,9 +1,56 @@
-import { DatePickerForm } from '@/components/DatePicker'
+import {
+  TableHeader,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from '@/components/ui/table'
+import { Table } from 'lucide-react'
+import React from 'react'
 
-export default function Indicated() {
+export default async function Indicated() {
+  const result = await fetch(
+    'https://digitaltracer.ddns.com.br/v1/siac/shortage',
+  )
+
+  const indicated = await result.json()
+
   return (
-    <>
-      <DatePickerForm />
-    </>
+    <div className="p-6 max-w-7xl mx-auto space-y-4">
+      <div className="border rounded-lg p-2">
+        <Table>
+          <TableHeader>
+            <TableHead>QTD</TableHead>
+            <TableHead>SKU</TableHead>
+            <TableHead>Data da solicitação</TableHead>
+            <TableHead>Similares</TableHead>
+          </TableHeader>
+          <TableBody>
+            {indicated.length > 0
+              ? indicated.map((item, index) => (
+                  <TableRow key={item.NUM_FAB}>
+                    <TableCell>{index + 1}</TableCell>
+                    <TableCell>{item.NUM_FAB}</TableCell>
+                    <TableCell>{item.PrimeiroDeSOLICITADO}</TableCell>
+                    <TableCell width={20}>
+                      {item.integration?.product?.products_connect?.products?.map(
+                        (product, index) => (
+                          <React.Fragment key={product.num_factory}>
+                            <span>{product?.product_id}</span>
+                            {index <
+                              item.integration.product.products_connect.products
+                                .length -
+                                1 && <span className="separator">, </span>}
+                          </React.Fragment>
+                        ),
+                      )}
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
   )
 }
