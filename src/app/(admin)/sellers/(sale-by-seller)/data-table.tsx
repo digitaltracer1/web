b919042881cfd3d08741a2baa5edf1b1cef1ebe1
tiagoribeironfs1 <tiagoribeironfs1@gmail.com>
2024-02-21 18:57:07 -0300

    feat: create filter in fetch data

--- src/app/(admin)/sellers/(sale-by-seller)/data-table.tsx
+++ src/app/(admin)/sellers/(sale-by-seller)/data-table.tsx
@@ -16,15 +16,21 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+import { setCookie } from 'cookies-next'
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
   data: TData[]
+  selectFilter: (filter: string | null) => void
 }
 
 export function DataTable<TData, TValue>({
   columns,
   data,
+  selectFilter,
 }: DataTableProps<TData, TValue>) {
   const table = useReactTable({
     data,
@@ -33,50 +39,78 @@ export function DataTable<TData, TValue>({
     getPaginationRowModel: getPaginationRowModel(),
   })
 
+  const [filter, setFilter] = useState('value') // amount | value
+
+  const changeFilter = () => {
+    setFilter((prevFilter) => (prevFilter === 'amount' ? 'value' : 'amount'))
+    selectFilter(filter)
+    setCookie('salebySellerFilter', filter)
+  }
+
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext(),
-                        )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && 'selected'}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+    <div className="gap-4">
+      <div className="flex justify-center items-center ">
+        <Button variant="ghost" onClick={() => changeFilter()}>
+          <ChevronLeftIcon className="h-4 w-4" />
+        </Button>
+        <h2 className="mx-4 w-32 text-center">
+          {filter === 'value' ? 'Quantidade' : 'Valor'}
+        </h2>
+        <Button variant="ghost" onClick={() => changeFilter()}>
+          <ChevronRightIcon className="h-4 w-4" />
+        </Button>
+      </div>
+
+      <div className="rounded-md border  ">
+        <Table>
+          <TableHeader className="2xl:h-10">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  )
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className="2xl:h-10">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-lg text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   )
 }
